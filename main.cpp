43b8cd77bc197dea1f23fddb2fb7c4a31a6e9fb7
Anton <mr.alligator@yandex.ru>
2020-02-11 00:03:41 +0300

    Initial commit

---
+++ main.cpp
@@ -0,0 +1,16 @@
+#include <iostream>
+#include <vector>
+
+template <typename T, int value>
+class matrix
+{
+private:
+    std::vector<std::vector<T>> m;
+public:
+
+};
+
+int main() {
+
+    return 0;
+}
