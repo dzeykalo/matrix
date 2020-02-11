7ad07d59257edde6b94b1d56d8a7a73263bb3ab4
Anton <mr.alligator@yandex.ru>
2020-02-11 23:51:06 +0300

    refactoring

--- main.cpp
+++ main.cpp
@@ -1,6 +1,36 @@
 #include <iostream>
 #include <vector>
 
+template <typename T>
+
+class matrix_mmm
+{
+private:
+    T& mm;
+public:
+    matrix_mmm(T& r) : mm(r) {}
+    T& operator=(T val)
+    {
+        mm = val;
+        return mm;
+    }
+};
+
+template <typename T>
+class matrix_mm
+{
+private:
+    std::vector<T>& mm;
+public:
+    matrix_mm(std::vector<T>& r) : mm(r) {}
+    matrix_mmm<T> operator[](unsigned int y)
+    {
+        return mm.at(y);
+    }
+};
+
+
+
 template <typename T, T value>
 class matrix
 {
@@ -9,36 +39,33 @@ private:
     T default_val = value;
     size_t count;
 public:
+    matrix(unsigned int x, unsigned int y): count(0)
+    {
+        m.resize(x, std::vector<T>(y,value));
+    }
     matrix(): count(0)
     {
-      m.resize(100, std::vector<T>(100,value));
+        m.resize(100, std::vector<T>(100,value));
     }
     T size()
     {
-      return static_cast<T>(m.size());
+        return static_cast<T>(m.size());
     }
-    class matrix_mm
+    matrix_mm<T> operator[](unsigned int x)
     {
-    private:
-      std::vector<T>& mm;
-    public:
-      matrix_mm(std::vector<T>& r) : mm(r) {}
-      T& operator[](unsigned int y)
-      {
-        return mm.at(y);
-      }
-    };
-    matrix_mm operator[](unsigned int x)
+        return matrix_mm<T>(m.at(x));
+    }
+    T& operator=(T val)
     {
-      return matrix_mm(m.at(x));
+        return val;
     }
 };
 
 int main()
 {
-    matrix<int, -1> m(100,100);
+    matrix<int, -1> m;
     std::cout << m.size() << std::endl;
     m[1][2] = 4;
-    std::cout << m[1][2] << " " << m[1][3] << std::endl;
+    //std::cout << m[1][2] << " " << m[1][3] << std::endl;
     return 0;
 }
