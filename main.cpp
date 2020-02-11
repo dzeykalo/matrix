1bd28a2a284aa988b4830c2e8655f020ae498c7c
Anton Dzeykalo <adzeykalo@abagy.com>
2020-02-11 19:25:23 +0300

    add operator []

--- main.cpp
+++ main.cpp
@@ -1,16 +1,44 @@
 #include <iostream>
 #include <vector>
 
-template <typename T, int value>
+template <typename T, T value>
 class matrix
 {
 private:
     std::vector<std::vector<T>> m;
+    T default_val = value;
+    size_t count;
 public:
-
+    matrix(): count(0)
+    {
+      m.resize(100, std::vector<T>(100,value));
+    }
+    T size()
+    {
+      return static_cast<T>(m.size());
+    }
+    class matrix_mm
+    {
+    private:
+      std::vector<T>& mm;
+    public:
+      matrix_mm(std::vector<T>& r) : mm(r) {}
+      T& operator[](unsigned int y)
+      {
+        return mm.at(y);
+      }
+    };
+    matrix_mm operator[](unsigned int x)
+    {
+      return matrix_mm(m.at(x));
+    }
 };
 
-int main() {
-
+int main()
+{
+    matrix<int, -1> m(100,100);
+    std::cout << m.size() << std::endl;
+    m[1][2] = 4;
+    std::cout << m[1][2] << " " << m[1][3] << std::endl;
     return 0;
 }
