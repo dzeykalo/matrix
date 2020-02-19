aae80d3e8fd4dd289a4eb30d306816c99c48949b
Anton <mr.alligator@yandex.ru>
2020-02-20 00:21:26 +0300

    add class PushDef

--- matrix.h
+++ matrix.h
@@ -3,59 +3,61 @@
 #include <algorithm>
 
 template <typename T>
-class resizer
-{
-private:
-  std::vector<T>& v;
-public:
-  resizer(std::vector<T>& r) : v(r) {}
-  void resize(unsigned int size, T default_val){
-    v.resize(size+1, default_val);
-  }
-};
-
-template <typename T>
 struct element
 {
   int x;
   int y;
   T value;
+  element():x(-1), y(-1), value(0){};
+  element(int _x, int _y, T _value):x(_x), y(_y), value(_value){};
+  element(T _value):x(-1), y(-1), value(_value){};
 };
-template <typename T, T value>
+
+template <typename T, T DefValue>
+class PushDef
+{
+private:
+    std::vector<element<T>>& v;
+public:
+    PushDef(std::vector<element<T>>& r) : v(r) {
+    }
+    void Def(){
+      element<T> el(DefValue);
+      v.push_back(el);
+    }
+    void Try(){
+      if (v.back().value != DefValue){
+        Def();
+      }
+    }
+};
+
+template <typename T, T DefValue>
 class Matrix
 {
 private:
   std::vector<element<T>> m;
-  T def_val;
 public:
-  Matrix():def_val(value){
-    element<T> el;
-    el.x = el.y = -1; el.value = def_val;
-    m.push_back(el);
+  Matrix():m(){
+    PushDef<T,DefValue> Push(m);
+    Push.Def();
   };
   size_t size() {
-    if (m.back().value != def_val){
-      element<T> el;
-      el.x = el.y = -1; el.value = def_val;
-      m.push_back(el);
-    }
+    PushDef<T,DefValue> Push(m);
+    Push.Try();
     return m.size()-1;
   }
 
   class access{
   private:
     std::vector<element<T>>& v;
-    T& dv;
     unsigned int x;
   public:
-    access(std::vector<element<T>>& r, T& def_val, unsigned int _x) : v(r), dv(def_val), x(_x){}
+    access(std::vector<element<T>>& r, unsigned int _x) : v(r), x(_x){}
     T& operator[](unsigned int y){
-      if (v.back().value != dv){
-        element<T> el;
-        el.x = -1; el.y = -1; el.value = dv;
-        v.push_back(el);
-      }
-      auto result = std::find_if(v.begin(), v.end(), [=](element<T> el){ return (el.x == x && el.y == y && el.value != dv);});
+      PushDef<T,DefValue> Push(v);
+      Push.Try();
+      auto result = std::find_if(v.begin(), v.end(), [=](element<T> el){ return (el.x == x && el.y == y && el.value != DefValue);});
       if (result != v.end()){
         return (*result).value;
       }
@@ -64,7 +66,7 @@ public:
     }
   };
   access operator[](unsigned int x){
-    return access(m,def_val,x);
+    return access(m, x);
   }
 
   class Iterator{
